5a327ddd922a9bba7796172db9e045cce55580c2
Apoorv1998-Sopho <apoorvagni@hotmail.com>
2016-12-08 16:52:08 +0000

    Finished generate commenting

--- find/generate.c
+++ find/generate.c
@@ -34,7 +34,10 @@ int main(int argc, string argv[])
     // conversion of the 2nd argument to an int from a string.
     int n = atoi(argv[1]);
 
-    // if number of arguments is exactly 3, this block of code runs and produces a seed to be placed in drand48()
+    /** if number of arguments is exactly 3, srand48 is called with the second argument as srand48's argument.
+     * if the number of arguments is exactly 2, srand48 is called with time() as it's agument
+     * in both cases srand48 is called upon with some argument to be used to seed drand48()
+     */
     if (argc == 3)
     {
         srand48((long int) atoi(argv[2]));
@@ -44,10 +47,12 @@ int main(int argc, string argv[])
         srand48((long int) time(NULL));
     }
 
-    // TODO: comment me
+    /** Looping over the number of required random numbers we need to find.
+     * Printing ints that actually have been formed from type casting floats, produced by drand48().
+     */
     for (int i = 0; i < n; i++)
     {
-        printf("%i\n", (int) (drand48() * LIMIT));
+        printf("%d\n", (int)(drand48() * LIMIT));
     }
 
     // success
