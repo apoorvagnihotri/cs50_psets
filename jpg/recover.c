7736818c30633f6c9ac5c5e16613810f98a1aa8f
Apoorv1998-Sopho <apoorvagni@hotmail.com>
2016-12-14 10:55:55 +0000

    made recover.c, not tested

--- jpg/recover.c
+++ jpg/recover.c
@@ -1,13 +1,52 @@
-/**
- * recover.c
- *
- * Computer Science 50
- * Problem Set 4
- *
- * Recovers JPEGs from a forensic image.
- */
+#include <stdio.h>
+#include <stdint.h>
 
-int main(int argc, char* argv[])
+typedef uint8_t BYTE;
+
+int main (void)
 {
-    typedef struct
+    BYTE* inptr = malloc(sizeof(BYTE) * 512);
+    char* filename[4];
+    int counter = 0;
+    bool found = false;
+    FILE* infile = fopen("card.raw", "r");
+    if(infile == NULL) 
+    {
+        printf("Error in opening file");
+        return(-1);
+    }
+    
+    FILE* outfile = fopen(000.jpg, "w");
+    if(outfile == NULL) 
+    {
+        printf("Error in creating file");
+        return(-1);
+    }
+    
+    
+    while (true)
+    {
+        fread(inptr, sizeof(inptr), 1, infile);
+        if (((inptr[0] == ) && (inptr[1] == )) && (inptr[2] == ))
+        {            
+            if (write)
+            {
+                fclose(outfile)
+                counter++;
+                sprintf(filename, "%3d.jpg", counter);
+                fopen(filename, "w");   
+            }
+            found = true;   
+        }
+        if (found)
+        {
+            fwrite(inptr, sizeof(inptr), 1, outfile);
+        }
+        if (feof)
+        {
+            fclose(outfile);
+            fclose(infile);
+            break;
+        }
+    }
 }
