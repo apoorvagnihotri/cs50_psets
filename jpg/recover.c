fabbff1b786534400458df141357c780abd0fe43
Apoorv1998-Sopho <apoorvagni@hotmail.com>
2016-12-15 10:42:48 +0000

    save

--- jpg/recover.c
+++ jpg/recover.c
@@ -6,52 +6,52 @@ typedef uint8_t BYTE;
 
 int main (void)
 {
-    char* filename = "000.jpg";
+    char jpgout = "000.jpg";
     int counter = 0;
+    
+    //found is used to see if first jpg reached or not
     bool found = false;
-    FILE* infile = fopen("card.raw", "r");
-    if(infile == NULL) 
+
+    FILE* inptr = fopen("card.raw", "r");
+    if(inptr == NULL) 
     {
         printf("Error in opening file");
         return(-1);
     }
     
-    FILE* outfile = fopen(filename, "w");
-    if(outfile == NULL) 
+    FILE* outptr = fopen(jpgout, "w");
+    if(outptr == NULL) 
     {
         printf("Error in creating file");
         return(-1);
     }
     
     BYTE* buffer = malloc(sizeof(BYTE) * 512);
-    
-    
     while (true)
     {
-        fread(buffer, sizeof(buffer), 1, infile);
+        fread(buffer, sizeof(buffer), 1, inptr);
 
         if ((((buffer[0] == 0xff) && (buffer[1] == 0xd8)) && (buffer[2] == 0xff)) && (buffer[3] == 0xe0 || buffer[3] == 0xe1))
-        {            
-            
+        {
             if (found)
             {
-                fclose(outfile);
+                fclose(outptr);
                 counter++;
-                sprintf(filename, "%3d.jpg", counter);
-                fopen(filename, "w");
+                sprintf(jpgout, "%3d.jpg", counter);
+                outptr = fopen(jpgout, "w");
             }
             found = true;   
         }
         
         if (found)
         {
-            fwrite(buffer, sizeof(buffer), 1, outfile);
+            fwrite(buffer, sizeof(buffer), 1, outptr);
         }
         
-        if (feof(infile))
+        if (feof(inptr))
         {
-            fclose(outfile);
-            fclose(infile);
+            fclose(outptr);
+            fclose(inptr);
             break;
         }
     }
